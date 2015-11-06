8a5972f347f473415dfc31a30f40b6791fe6f430
Oliver Kett <oliver.kett@fau.de>
2015-11-06 12:51:28 +0100

    Initial Commit

---
+++ WifiToggle/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WifiToggle
+//
+//  Created by Kett, Oliver on 06.11.15.
+//  Copyright © 2015 Kett, Oliver. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
